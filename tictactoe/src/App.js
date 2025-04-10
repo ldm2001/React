6aeba7aa078380139bf52657b160010ff8b36332
ldm2001 <01ldm@naver.com>
2025-04-10 16:50:10 +0900

    Commit

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,45 +1,5 @@
+import Board from './Board';
 import './App.css';
-import { useState } from 'react';
-
-function Square() {
-  const [value, setValue] = useState(null);
-  function handleClick() {
-    setValue("x")
-  }
-
-  return (
-    <div>
-      <button
-        className="square"
-        onClick={handleClick}
-      >
-        {value}
-      </button>
-    </div>
-  );
-}
-
-function Board() {
-  return (
-    <>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-    </>
-  );
-}
 
 export default function App() {
   return (
---
+++ tictactoe/src/Square.js
@@ -0,0 +1,9 @@
+export default function Square({ value, onSquareClick }) {
+  return (
+    <div>
+      <button className="square" onClick={onSquareClick}>
+        {value}
+      </button>
+    </div>
+  );
+}
