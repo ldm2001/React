5dc880789f30a4fd8e913d12a88bf78a86c8fcd7
ldm2001 <01ldm@naver.com>
2025-04-18 15:51:40 +0900

    Commit

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,11 +1,11 @@
-import Board from './Board';
 import './App.css';
+import Game from './Game';
 
 export default function App() {
   return (
     <div>
       <h1>Tic Tac Toe</h1>
-      <Board />
+      <Game />
     </div>
   );
 }
--- tictactoe/src/Square.js
+++ tictactoe/src/Square.js
@@ -1,9 +1,9 @@
 export default function Square({ value, onSquareClick }) {
   return (
-    <div>
+    <>
       <button className="square" onClick={onSquareClick}>
         {value}
       </button>
-    </div>
+    </>
   );
 }
