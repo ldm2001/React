99af720a996d54f5cacf2d28f03b5776293ffeb4
ldm2001 <01ldm@naver.com>
2025-05-08 15:56:10 +0900

    Commit

---
+++ category/src/App.js
@@ -0,0 +1,38 @@
+export default function App() {
+  return (
+    <>
+      <FilterableProductTable products={PRODUCTS} />
+    </>
+  )
+}
+
+function FilterableProductTable({ products }) {
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable products={products} />
+    </div>
+  );
+}
+
+function SearchBar() {
+  return (
+    <form>
+      <input type="text" placeholder="Search..." />
+      <label>
+        <input type="checkbox" />
+        {' '}
+        Only show products in stock
+      </label>
+    </form>
+    );
+  }
+
+  const PRODUCTS =  [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+];
